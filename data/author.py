c1e21c9ee141f6f3fadd86a69601393683f4f50e
Natasha-rare <natasha_ea5@mail.ru>
2020-04-05 09:34:27 +0300

    исправлены годы жизни автора (теперь строка # 1895-1925)

--- data/author.py
+++ data/author.py
@@ -10,9 +10,9 @@ class Author(SqlAlchemyBase, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    years = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)  # сколько лет прожил
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)  # имя
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)   # фамилия
+    years = sqlalchemy.Column(sqlalchemy.String, nullable=False)  # годы жизни
     list_of_books = sqlalchemy.Column(sqlalchemy.String, nullable=False)  # список книг
 
     books = orm.relation("Books", back_populates='author')  # связывем книгу с автором
